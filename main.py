1baa0f15b28f2e999c317979014a6eb8f3ec9988
Алексей <131593418+alexbrenson@users.noreply.github.com>
2023-05-07 16:51:15 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,25 @@
-
+from instr import*
+from second_win import*
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QLabel, QVBoxLayout, QHBoxLayout, QGroupBox, QRadioButton, QListWidget, QLineEdit
+class MainWin(QWidget):
+    def set_appear(self):
+        self.setWindowTitle(txt_title)
+        self.resize(win_height, win_width)
+        self.move(win_x, win_y)
+    def initUI(self):
+        self.hello_text = QLabel(txt_hello)
+        self.instruction = QLabel(txt_instruction)
+        self.button = QPushButton(txt_next)
+        self.layout = QVBoxLayout()
+        self.layuot.addWidget(self.hello.text)
+        self.layout.addWidget(self.instryction)
+        self.layout.addWidget(self.button)
+    def connects(self):
+        self.btn_text.clicked.connect(self.next_click)
+    def next_click(self):
+        self.hide()
+        self.tw = TestWin()
+app = QApplication([])
+mw = MainWin()
+app.exec_()
