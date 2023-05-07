534395b33550d610daf340ec90ee3eda00fdb71b
Алексей <131593418+alexbrenson@users.noreply.github.com>
2023-05-07 16:53:13 +0500

    Create instryctions.py

--- instryctions.py
+++ instryctions.py
@@ -1,1 +1,25 @@
+win_x, win_y = 200, 100
+win_width, win_height = 1000, 600
 
+txt_hello = 'Добро пожаловать в программу по определению здоровья'
+txt_next = 'Начать'
+txt_instruction = '' 
+txt_title = 'Здоровье'
+txt_name = ''
+txt_hintname = ""
+txt_hintage = ""
+txt_test1 = ''
+txt_test2 = ''
+txt_test3 = ''
+txt_sendresults = 'Отправить результаты'
+txt_hinttest1 = '0'
+txt_hinttest2 = '0'
+txt_hinttest3 = '0'
+txt_starttest1 = 'Начать первый тест'
+txt_starttest2 = 'Начать делать приседания'
+txt_starttest3 = 'Начать финальный тест'
+
+txt_age = 'Полных лет:'
+txt_finalwin = ''
+txt_index = 'Индекс Руфье'
+txt_workheart = 'Работоспособность сердца:'
