b1b1c5146b74c5dc7aeaf60fbf5d6db6f2b52c6b
Алексей <131593418+alexbrenson@users.noreply.github.com>
2023-05-14 16:51:39 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
-from instr import*
-from second_win import*
 from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QLabel, QVBoxLayout, QHBoxLayout, QGroupBox, QRadioButton, QListWidget, QLineEdit
+from instr import *
+from second_win import *
 class MainWin(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.set_appear
+        self.initUI
+        self.connects
+        self.next_click
     def set_appear(self):
         self.setWindowTitle(txt_title)
         self.resize(win_height, win_width)
